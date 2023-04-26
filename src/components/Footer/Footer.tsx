26df455aa67bb0c495b610951f120906aea84d69
Kev <sulpeum@gmail.com>
2023-04-26 15:05:43 -0400

    update footer link to open in new tab

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <FooterContainer>
         <p className="font-semibold text-sm">
           <span className="px-8 py-4 bg-dark text-white rounded">
-            <Link href="https://kvo.vercel.app/" className="text-accent">
+            <Link href="https://kvo.vercel.app/" className="text-accent" target="_blank">
               Designed and developed by Kevin Vo
             </Link>
           </span>
