e6b96b08bcffb471d1561891ec303632db5b42bf
Kev <sulpeum@gmail.com>
2023-04-28 17:45:53 -0400

    add jotai state management

---
+++ src/atoms/globals.tsx
@@ -0,0 +1,6 @@
+import { atom } from "jotai"
+
+export const decadeAtom = atom<string | null>("2030")
+export const nameAtom = atom<string | null>("Acevedo-Kennedy")
+export const categoryAtom = atom<string | null>("Energy")
+export const locationAtom = atom<string | null>("All")
