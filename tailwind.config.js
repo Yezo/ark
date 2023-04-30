ec8ab491360c1964d1b332d7e3ba2900cb575aa9
Kev <sulpeum@gmail.com>
2023-04-30 14:15:26 -0400

    tailwind updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,28 @@ module.exports = {
         "light-text": "#F6F6F6",
         "dark-bg": "#212121",
         "dark-text": "#333333",
+        muted: "#A7B0BD",
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [require("tailwind-scrollbar-hide", "tailwindcss-animate")],
 }
