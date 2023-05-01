fdabfb00e65ded192c1af07b843074198cdfe053
Kev <sulpeum@gmail.com>
2023-04-30 20:14:19 -0400

    add accent border

--- src/components/HeroSection/HeroBanner.tsx
+++ src/components/HeroSection/HeroBanner.tsx
@@ -4,7 +4,12 @@ import hero from "../../../public/hero.webp"
 export default function HeroBanner() {
   return (
     <div className="basis-1/2">
-      <Image src={hero} alt="Image of climate change" placeholder="blur" />
+      <Image
+        src={hero}
+        alt="Image of climate change"
+        placeholder="blur"
+        className="border-4 border-accent"
+      />
     </div>
   )
 }
